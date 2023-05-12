b8a3656217b334237b76de24080a334d56e02bdf
Ben Evans <benjamin.john.evans@gmail.com>
2023-05-12 19:34:25 +0200

    Revert Caching

--- src/main/java/oreilly/caching/Caching.java
+++ src/main/java/oreilly/caching/Caching.java
@@ -7,13 +7,13 @@ public class Caching {
 
     private void run() {
         for (int i = 0; i < 10_000; i += 1) {
-            touchEvery4thLine();
+            touchEveryLine();
             touchEveryItem();
         }
         System.out.println("Line     Item");
         for (int i = 0; i < 100; i += 1) {
             long t0 = System.nanoTime();
-            touchEvery4thLine();
+            touchEveryLine();
             long t1 = System.nanoTime();
             touchEveryItem();
             long t2 = System.nanoTime();
@@ -28,8 +28,8 @@ public class Caching {
             testData[i] += 1;
     }
 
-    private void touchEvery4thLine() {
-        for (int i = 0; i < testData.length; i += 64)
+    private void touchEveryLine() {
+        for (int i = 0; i < testData.length; i += 16)
             testData[i] += 1;
     }
 
