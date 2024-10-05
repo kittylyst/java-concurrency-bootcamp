d991ac89843cd19e9d66a612125113a5f638c77f
Ben Evans <benjamin.john.evans@gmail.com>
2024-10-05 17:06:30 +0200

    Undo maps changes

--- src/main/java/oreilly/maps/MapExamples.java
+++ src/main/java/oreilly/maps/MapExamples.java
@@ -1,14 +1,11 @@
 package oreilly.maps;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class MapExamples {
     public static void main(String[] args) {
-        Map<String, String> map = Collections.synchronizedMap(new HashMap<>());
-        int SIZE = 10_000_000;
+        Map<String, String> map = new SimpleDict();
+        int SIZE = 10_000;
 
         Runnable r1 = () -> {
           for (int i = 0; i < SIZE; i = i + 1) {
