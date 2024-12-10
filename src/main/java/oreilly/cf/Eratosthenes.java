5d1c2cd4c80daa5368549e0eae53246c0d1509fe
Ben Evans <benjamin.john.evans@gmail.com>
2024-12-10 12:49:16 +0100

    Move for merge 2024 12 10 (#8)
    * After session
    
    * Tidy up, merge
    
    * Undo maps changes
    
    * WIP
    
    * Fix accidental change

--- src/main/java/oreilly/cf/Eratosthenes.java
+++ src/main/java/oreilly/cf/Eratosthenes.java
@@ -2,6 +2,11 @@ package oreilly.cf;
 
 public class Eratosthenes {
     public static long findPrime(int n) {
+        try {
+            Thread.sleep((long) (2000 * Math.random()));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return 0L;
     }
 }
